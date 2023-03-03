4c1da0a7adcb812d7039fd213af89b0485e84401
Daniel Rodriguez <breakpoint0341@gmail.com>
2023-03-03 23:42:21 +0000

    Creacion de metadata file en base al parametro IN

--- sendPrescription.sh
+++ sendPrescription.sh
@@ -6,24 +6,35 @@
 #                           MAINNET:    https://github.com/gitmachtl/scripts/tree/master/cardano/mainnet
 
 #Check the commandline parameter
-if [[ $# -eq 1 && ! $1 == "" ]]; then metadatafile="$(dirname $1)/$(basename $1 .json)"; metadatafile=${metadatafile/#.\//}; else echo "ERROR - Usage: $0 <metadata file name>"; exit 2; fi
+if [[ $# -eq 1 && ! $1 == "" ]]; then receta_id="$(dirname $1)/$(basename $1 .json)"; receta_id=${receta_id/#.\//}; else echo "ERROR - Usage: $0 <receta_id>"; exit 2; fi
 
 #send metadata file into a variable
-METADATA_FILE="${metadatafile}.json"
+METADATA_FILE="${receta_id}.json"
 
-#Check if metadatafile file exists, create a transacion with metadata added
-if [ -f "${metadatafile}.json" ]; then
+#Check if receta_id not file exists, I create a metadata file 
+if [ ! -f "${receta_id}.json" ]; then
     echo 'Información de Metadata';
 
     echo $METADATA_FILE;
-    cardano-cli query tip --testnet-magic 2;
+    cardano-cli query tip --testnet-magic 2;  
 
-    #typeOfAddr=$(cardano-cli query utxo --testnet-magic 2 --address $ADDRESS);
-    #echo $typeOfAddr;
-    #if [[ ${typeOfAddr} == ${addrTypePayment} || ${typeOfAddr} == ${addrTypeStake} ]]; then echo "$(basename ${addrName})" > ${tempDir}/tempAddr.addr; addrName="${tempDir}/tempAddr";
+    #create metadata file based on parameter
+    cat > $METADATA_FILE <<EOF
+    {
+        "${receta_id}": {
+            "name": "Receta Electronica Citaldoc",
+            "Rp": "Ácido acetil Salisílico (Deliciosa Aspirina) 500mg.",
+            "Indicaciones": "Clavátela 28 veces al dia por dos x 2 meses a la misma hora."
+        }
+    }
+EOF
 
+    echo "------------------------------------------------------------"
+   
+    echo  "Creado Archivo JSON.";
+    echo  "Script Completado.";
 
-    #cardano-cli query utxo --testnet-magic 2 --address $ADDRESS
-    ./01_sendLovelaces.sh my_address other_address 'MIN' $METADATA_FILE;
-
+    #./01_sendLovelaces.sh my_address other_address 'MIN' $METADATA_FILE;
+else
+    echo "El archivo de metadata para la receta ID = ${receta_id} ya existe.";
 fi
