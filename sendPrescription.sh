45f8d9ba0643a86a2d6417ddfc7396b0905c0c85
Daniel Rodriguez <breakpoint0341@gmail.com>
2023-03-03 21:51:59 +0000

    llamada a sendPrescription c/"metada file"  param

--- sendPrescription.sh
+++ sendPrescription.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Script is brought to you by DANIEL WEB3 RODRIGUEZ, Telegram @BreakpointDaniel
-
+# Stakepool scripts ATADA : 
+#                           TESTNET:    https://github.com/gitmachtl/scripts/tree/master/cardano/testnet
+#                           MAINNET:    https://github.com/gitmachtl/scripts/tree/master/cardano/mainnet
 
 #Check the commandline parameter
-if [[ $# -eq 1 && ! $1 == "" ]]; then addrName="$(dirname $1)/$(basename $1 .addr)"; addrName=${addrName/#.\//}; else echo "ERROR - Usage: $0 <AdressName or HASH or '\$adahandle'>"; exit 2; fi
+if [[ $# -eq 1 && ! $1 == "" ]]; then metadatafile="$(dirname $1)/$(basename $1 .json)"; metadatafile=${metadatafile/#.\//}; else echo "ERROR - Usage: $0 <metadata file name>"; exit 2; fi
 
-#send address into a variable
-#ADDRESS="${addrName}"
-ADDRESS="addr_test1vp2l080u4lzhwvfq7exlncw2tye2crr5wl9f0vxgh9ewctsy5dk43";
+#send metadata file into a variable
+METADATA_FILE="${metadatafile}.json"
 
-#Check if addrName file does not exists, make a dummy one in the temp directory and fill in the given parameter as the hash address
-if [ ! -f "${addrName}.addr" ]; then
-    echo 'Información de TIP';
+#Check if metadatafile file exists, create a transacion with metadata added
+if [ -f "${metadatafile}.json" ]; then
+    echo 'Información de Metadata';
 
-    echo $ADDRESS;
+    echo $METADATA_FILE;
     cardano-cli query tip --testnet-magic 2;
 
-    typeOfAddr=$(cardano-cli query utxo --testnet-magic 2 --address $ADDRESS);
-    echo $typeOfAddr;
+    #typeOfAddr=$(cardano-cli query utxo --testnet-magic 2 --address $ADDRESS);
+    #echo $typeOfAddr;
     #if [[ ${typeOfAddr} == ${addrTypePayment} || ${typeOfAddr} == ${addrTypeStake} ]]; then echo "$(basename ${addrName})" > ${tempDir}/tempAddr.addr; addrName="${tempDir}/tempAddr";
 
 
     #cardano-cli query utxo --testnet-magic 2 --address $ADDRESS
+    ./01_sendLovelaces.sh my_address other_address 'MIN' $METADATA_FILE;
 
 fi
